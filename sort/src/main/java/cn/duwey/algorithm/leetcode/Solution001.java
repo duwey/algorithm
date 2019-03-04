5741550f4877eee0eca20fd71fd96c224dc8c619
duwey <v_duwei@saicmotor.com>
2019-03-04 17:47:31 +0800

    两数之和

---
+++ sort/src/main/java/cn/duwey/algorithm/leetcode/Solution001.java
@@ -0,0 +1,31 @@
+package cn.duwey.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ */
+public class Solution001 {
+
+    /**
+     * 将每个元素的值和它的索引添加到表中。检查每个元素所对应的目标元素（target - nums[i]）是否存在于表中。
+     * 在进行迭代并将元素插入到表中的同时，检查表中是否已经存在当前元素所对应的目标元素。如果它存在，那我们已经找到了对应解，并立即将其返回。
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No solution");
+    }
+
+}
